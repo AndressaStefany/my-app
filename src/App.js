ab0cfb4dda0aee811d17419d7dc63669cee44640
AndressaStefany <andressa.stefany@yahoo.com.br>
2018-04-05 12:55:50 -0300

    router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { HashRouter as Router, Route, Link } from 'react-router-dom'
 import AppBar from 'material-ui/AppBar'
 import Drawer from 'material-ui/Drawer'
 import MenuItem from 'material-ui/MenuItem'
@@ -8,6 +8,8 @@ import IconMenu from 'material-ui/IconMenu'
 import FlatButton from 'material-ui/FlatButton'
 import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert'
 import RaisedButton from 'material-ui/RaisedButton'
+import PageQuestion from "./components/PageQuestion";
+import Home from "./components/Home";
 
 class Login extends React.Component{
     static muiName = 'FlatButton';
@@ -51,67 +53,62 @@ class App extends React.Component {
             open: false,
             logged: true // see false
         }
-        // this.toggleDrawer = this.toggleDrawer.bind(this);
     }
 
     toggleDrawer = () => this.setState({ open: !this.state.open });
 
     render() {
         return (
-            <div>
-                <AppBar
-                    title="Title"
-                    titleStyle={styles.buttonsAppBar}
-                    className='appBar'
-                    iconClassNameRight="muidocs-icon-navigation-expand-more"
-                    onLeftIconButtonClick={this.toggleDrawer}
-                    iconStyleLeft={{background:'#b09b5b'}}
-                    iconElementRight={this.state.logged ? <Logged /> : <Login />}
-                />
-
-                <Drawer
-                    docked={false}
-                    width={260}
-                    onRequestChange={this.toggleDrawer}
-                    open={this.state.open}
-                >
+            <Router>
+                <div>
                     <AppBar
                         title="Title"
                         titleStyle={styles.buttonsAppBar}
                         className='appBar'
+                        iconClassNameRight="muidocs-icon-navigation-expand-more"
                         onLeftIconButtonClick={this.toggleDrawer}
                         iconStyleLeft={{background:'#b09b5b'}}
+                        iconElementRight={this.state.logged ? <Logged /> : <Login />}
                     />
-                    <MenuItem
-                        primaryText="Home"
-                        containerElement={<Link to="/" />}
-                        onClick={() => {
-                            console.log('going home')
-                            alert('going home!')
-                            this.toggleDrawer()
-                        }}
-                    />
-                    <MenuItem
-                        primaryText="about"
-                        containerElement={<Link to="/viewquestions" />}
-                        onClick={() => {
-                            console.log('viewquestions')
-                            alert('going to about page!')
-                            this.toggleDrawer()
-                        }}
-                    />
-                </Drawer>
 
-                <div style={{ textAlign: 'center' }}>
-                    {this.props.children}
+                    <Drawer
+                        docked={false}
+                        width={260}
+                        onRequestChange={this.toggleDrawer}
+                        open={this.state.open}
+                    >
+                        <AppBar
+                            title="Title"
+                            titleStyle={styles.buttonsAppBar}
+                            className='appBar'
+                            onLeftIconButtonClick={this.toggleDrawer}
+                            iconStyleLeft={{background:'#b09b5b'}}
+                        />
+                        <MenuItem
+                            primaryText="Home"
+                            containerElement={<Link to="/" />}
+                            onClick={() => {
+                                this.toggleDrawer()
+                            }}
+                        />
+                        <MenuItem
+                            primaryText="pagequestion"
+                            containerElement={<Link to="/pagequestion" />}
+                            onClick={() => {
+                                this.toggleDrawer()
+                            }}
+                        />
+                    </Drawer>
 
-                    {/*<RaisedButton*/}
-                        {/*label="Toggle Drawer"*/}
-                        {/*onClick={this.toggleDrawer}*/}
-                    {/*/>*/}
+                    <div style={{ textAlign: 'center' }}>
+                        {this.props.children}
+                    </div>
+
+                    <Route exact path='/' component={Home}/>
+                    <Route path='/pagequestion' component={PageQuestion}/>
+
                 </div>
-
-            </div>
+            </Router>
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Router, Route, IndexRoute, HashRouter, Link } from 'react-router-dom'
+import HashRouter from 'react-router-dom'
 import injectTapEventPlugin from 'react-tap-event-plugin'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
 import App from './App'
-import PageQuestion from './components/PageQuestion'
-import ViewQuestions from './components/ViewQuestions'
-
-import TopBar from './components/TopBar'
 
 import 'material-grid/dist/css/material-grid.css'
 import './index.css'
@@ -17,20 +13,9 @@ injectTapEventPlugin();
 
 const Root = () => (
     <MuiThemeProvider >
-        {/*<TopBar style={{zDepth:'6dp'}} title={<span className='titleStyles'>Title</span>}/>*/}
-        {/*<Router>*/}
-            {/*<div>*/}
-                {/*<ul>*/}
-                    {/*<li><Link to="/">Page</Link></li>*/}
-                    {/*<li><Link to="/viewquestions">View</Link></li>*/}
-                {/*</ul>*/}
-                <HashRouter>
-                    <Route  path="/" component={App} />
-                    {/*<Route exact path="/" component={PageQuestion} />*/}
-                    {/*<Route exact path="/viewquestions" component={ViewQuestions} />*/}
-                </HashRouter>
-            {/*</div>*/}
-        {/*</Router>*/}
+        <HashRouter>
+            <App />
+        </HashRouter>
     </MuiThemeProvider>
 );
 
